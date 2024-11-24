f3754040bfcfbd5bdd5afab78d7d45a5ba274b07
sv.konoplitskii <s.konoplitsky@yandex.com>
2024-11-24 21:23:05 +0300

    feat: 'delete other files'

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
+  const ideas = [
+    { nick: 'cool-idea-1', name: 'Idea 1', description: 'Description of idea 1' },
+    { nick: 'cool-idea-2', name: 'Idea 2', description: 'Description of idea 2' },
+    { nick: 'cool-idea-3', name: 'Idea 3', description: 'Description of idea 3' },
+    { nick: 'cool-idea-4', name: 'Idea 4', description: 'Description of idea 4' },
+    { nick: 'cool-idea-5', name: 'Idea 5', description: 'Description of idea 5' },
+  ]
   return (
     <>
+      <h1>Idea List</h1>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {ideas.map((idea) => {
+          return (
+            <div key={idea.nick}>
+              <h2>{idea.name}</h2>
+              <p>{idea.description}</p>
+            </div>
+          )
+        })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
